5168f1a52e8eba0139ca770e5ba7923a8593f739
Jesus De La Rosa <budderlod@gmail.com>
2022-11-28 01:12:55 -0500

    EVERYTHING IS BREAKING FOR NO REASON

--- .gameFiles/dirs/deleteData.sh
+++ .gameFiles/dirs/deleteData.sh
@@ -2,13 +2,9 @@
 # deletes any data. includes basically just the clues the user has found + any visited flags
 
 shipHub=~/LarkProject/.gameFiles/shipHub
-flagFile=$(find $shipHub -name .visitedFlag)
 clueFile=~/LarkProject/.gameFiles/pockets/foundClues/*
 
-if [ $flagFile ]
-then
-  rm $flagFile
-fi
+find $shipHub -name .visitedFlag -exec rm
 
 if [ -f $clueFile ]
 then
