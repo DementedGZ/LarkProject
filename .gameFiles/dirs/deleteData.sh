064d0b873d2382c201d2cbdf2e88bb6248de99f2
Jesus De La Rosa <budderlod@gmail.com>
2022-11-28 02:02:38 -0500

    HOW DIDI FIX THE BUGS HOYL SHIT

--- .gameFiles/dirs/deleteData.sh
+++ .gameFiles/dirs/deleteData.sh
@@ -4,7 +4,7 @@
 shipHub=~/LarkProject/.gameFiles/shipHub
 clueFile=~/LarkProject/.gameFiles/pockets/foundClues/*
 
-find $shipHub -name .visitedFlag -exec rm
+find "$shipHub" -name ".visitedFlag" -delete
 
 if [ -f $clueFile ]
 then
