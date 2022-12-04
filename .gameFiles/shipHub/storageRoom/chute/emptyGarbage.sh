67b409dba119ba69526b010d65ebd5ae3857abbd
Jesus De La Rosa <budderlod@gmail.com>
2022-12-03 22:09:26 -0500

    quick storage test+small changes

--- .gameFiles/shipHub/storageRoom/chute/emptyGarbage.sh
+++ .gameFiles/shipHub/storageRoom/chute/emptyGarbage.sh
@@ -7,17 +7,13 @@ NC=$(tput sgr0)
 clear
 if [ -f garbage1 ] && [ -f garbage2 ] && [ -f garbage3 ]  && [ -f garbage4 ];
 then
-<<<<<<< HEAD
 	cat ../../../clues/storageClue
 	cp ../../../clues/storageClue pockets/storageClue
 
-	echo "[${RED}Security Clue${NC}has been found! You can access it at any time via the ${BLU}foundClues ${NC}directory in your pocket.]"
-=======
-	cat ../../../clues/storageClue;
-	cp ../../../clues/storageClue pockets/storageClue;
-	echo "[${RED}Security Clue ${NC}has been found! You can access it at any time via the ${BLU}foundClues ${NC}directory in your pocket.]"
->>>>>>> b357a613a2353b016f75d0ebce4b592a76445475
+	echo "[${RED}Security Clue${NC} has been found! You can access it at any time via the ${BLU}foundClues ${NC}directory in your pocket.]"
 else
-	echo "Doesn't seem like I've cleared all the trash. Let me get back to it.";
+	echo
+	echo "Wish I could stop here, but there are clearly still a few pieces of trash scattered around."
+	echo "It's just over there. Imposter to stop. Potential evidence. Just gotta... move it on over here. Yeah."
+	echo
 fi
-		
