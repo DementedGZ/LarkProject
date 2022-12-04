c88671ca0dcff8788768af30044b47dec0298963
Jesus De La Rosa <budderlod@gmail.com>
2022-12-03 19:33:06 -0500

    theres alot here ok

--- .gameFiles/dirs/dirMovement.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# just makes a menu that shows where the player can go. intended to show up everytime they enter the hub area
-# can also be adjusted to show up everytime the player opens their map, if yall think thats cool
-
-clear
-cat dirs.txt
