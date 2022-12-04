4b4d081a8ffed74510d520d147b409add4e5c9e9
Kenneth <kennethm0124@gmail.com>
2022-12-03 21:37:53 -0500

    storage room task done. should teach the concept of the mv command fully.

--- .gameFiles/shipHub/storageRoom/chute/emptyGarbage.sh
+++ .gameFiles/shipHub/storageRoom/chute/emptyGarbage.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
+
+RED=$(tput setaf 1)
+BLU=$(tput setaf 4)
+NC=$(tput sgr0)
+
 clear
 if [ -f garbage1 ] && [ -f garbage2 ] && [ -f garbage3 ]  && [ -f garbage4 ];
 then
-	echo "Among the rubble you find insert clue here";
-	echo "There should also be some interesting dialog here with some functions but i'm to stupid to write them."; 
+	cat ../../../clues/storageClue;
+	cp ../../../clues/storageClue pockets/storageClue;
+	echo "[${RED}Security Clue ${NC}has been found! You can access it at any time via the ${BLU}foundClues ${NC}directory in your pocket.]"
 else
 	echo "Doesn't seem like I've cleared all the trash. Let me get back to it.";
 fi
